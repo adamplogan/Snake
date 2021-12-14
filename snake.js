87d45b32ceaa468da6c4f262543550959918a635
Adam <81320110+adamplogan@users.noreply.github.com>
2021-12-13 22:40:24 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -16,11 +16,11 @@ function openFullscreen(){
 }
 
 let defaultSize = {
-    x : 45,
-    y : 30,
-    b : 32,
-    xh : 22,
-    yh : 15
+    x : 22,
+    y : 15,
+    b : 64,
+    xh : 11,
+    yh : 7
 };
 
 //make box
@@ -66,12 +66,12 @@ die.src = "audio/die.mp3";
 var mute = document.getElementById("mute");
 
 
-eat1.volume = 0.3;
-eat2.volume = 0.3;
-eat3.volume = 0.3;
-eat4.volume = 0.3;
-eat5.volume = 0.3;
-die.volume = 0.3;
+eat1.volume = 0.05;
+eat2.volume = 0.05;
+eat3.volume = 0.05;
+eat4.volume = 0.05;
+eat5.volume = 0.05;
+die.volume = 0.05;
 
 //make snake
 
@@ -88,6 +88,11 @@ var modeSwitch = false;
 var pathGone = false;
 
 var snakeOnLine = true;
+
+var hamilton = true;
+
+var max = false;
+
 
 
 snake[0] = {
@@ -142,6 +147,71 @@ function direction(event){
         direction = "DOWN";
     }
 }
+
+var hamCycle = [];
+
+// Structure Hamiltonian Cycle Manually
+for(var i = 21; i > 0; i--){
+    hamCycle.push([i*box, 15*box]);
+}
+for(var i = 16; i < 30; i++){
+    if(i % 2 == 0){
+        for(var j = 1; j < 45; j++){
+            hamCycle.push([j*box, i*box]);
+        }
+    }
+    else{
+        for(var k = 44; k > 0; k--){
+            hamCycle.push([k*box, i*box]);
+        }
+    }
+}
+for(var i = 29; i >= 0; i--){
+    hamCycle.push([0*box, i*box]);
+}
+for(var i = 0; i < 15; i++){
+    if(i % 2 == 0){
+        for(var j = 1; j < 45; j++){
+            hamCycle.push([j*box, i*box]);
+        }
+    }
+    else{
+        for(var k = 44; k > 0; k--){
+            hamCycle.push([k*box, i*box]);
+        }
+    }
+}
+for(var i = 44; i > 21; i--){
+    hamCycle.push([i*box, 15*box]);
+}
+var hamCycle2 = [];
+var hamCycle2Nums = [[251, 250, 249, 242, 241, 236, 235, 234, 227, 226, 223, 222, 191, 190, 181, 180, 177, 176, 161, 160, 149, 148],
+                    [252, 247, 248, 243, 240, 237, 232, 233, 228, 225, 224, 221, 192, 189, 182, 179, 178, 175, 162, 159, 150, 147],
+                    [253, 246, 245, 244, 239, 238, 231, 230, 229, 218, 219, 220, 193, 188, 183, 184, 173, 174, 163, 158, 151, 146],
+                    [254, 255, 256, 207, 208, 209, 210, 211, 212, 217, 216, 195, 194, 187, 186, 185, 172, 171, 164, 157, 152, 145],
+                    [259, 258, 257, 206, 205, 204, 203, 202, 213, 214, 215, 196, 131, 132, 133, 134, 169, 170, 165, 156, 153, 144],
+                    [260,  41,  42,  43,  44,  45,  46, 201, 200, 199, 198, 197, 130, 129, 128, 135, 168, 167, 166, 155, 154, 143],
+                    [261,  40,  37,  36,  35,  34,  47,  48,  49,  50,  51,  52,  53,  54, 127, 136, 137, 138, 139, 140, 141, 142], 
+                    [262,  39,  38, 291, 292,  33,  32,  31,  30,  29,  28, 329, 328,  55, 126, 125, 124, 123, 122, 121, 120, 119],
+                    [263, 276, 277, 290, 293,  22,  23,  24,  25,  26,  27,   0, 327,  56, 111, 112, 113, 114, 115, 116, 117, 118],
+                    [264, 275, 278, 289, 294,  21,  20,  19,  18,  17,  16,   1, 326,  57, 110, 109, 102, 101,  96,  95,  90,  89],
+                    [265, 274, 279, 288, 295,  10,  11,  12,  13,  14,  15,   2, 325,  58, 107, 108, 103, 100,  97,  94,  91,  88],
+                    [266, 273, 280, 287, 296,   9,   8,   7,   6,   5,   4,   3, 324,  59, 106, 105, 104,  99,  98,  93,  92,  87],
+                    [267, 272, 281, 286, 297, 302, 303, 314, 315, 316, 317, 318, 323,  60,  79,  80,  81,  82,  83,  84,  85,  86],
+                    [268, 271, 282, 285, 298, 301, 304, 313, 312, 311, 310, 319, 322,  61,  78,  77,  76,  75,  74,  73,  72,  71],
+                    [269, 270, 283, 284, 299, 300, 305, 306, 307, 308, 309, 320, 321,  62,  63,  64,  65,  66,  67,  68,  69,  70]];
+
+
+for(var k = 0; k <= 330; k++){
+    for(var i = 0; i < 15; i++){
+        for(var j = 0; j < 22; j++){
+            if(hamCycle2Nums[i][j] == k){
+                hamCycle2.push([j*box, i*box]);
+            }
+        }
+    }
+}
+
 
 // collision function
 function collision(head, array){
@@ -228,11 +298,14 @@ function draw(){
     }
 
         for(let i = 0; i < snake.length; i++){
+
+            
             ctx.fillStyle = (i == 0)? headColor : snakeColor;
             ctx.fillRect(snake[i].x, snake[i].y, box, box);
         
             ctx.strokeStyle = "black";
             ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+
         }
     
 
@@ -733,50 +806,234 @@ if(mode == 2 || mode == 3){
         }
         
     }
-//Hamiltonian Cycle
 if(mode == 4){
+
+    if(score == 327){
+        for(var x = 0; x < (defaultSize.x); x++){
+            for(var y = 0; y < (defaultSize.y); y++){
+                ctx.fillStyle = (i == 0)? headColor : snakeColor;
+                ctx.fillRect(x*box, y*box, box, box);
+            
+                ctx.strokeStyle = "black";
+                ctx.strokeRect(x*box, y*box, box, box);
+            }
+        }
+        max = true;
+        clearInterval(game);
+    }
+
+    if(sum == 330){
+        sum = 0
+    }
+    snakeX = hamCycle2[sum][0];
+    snakeY = hamCycle2[sum][1];
+    sum++;
+  
+}
+//Hamiltonian Cycle
+if(mode == 5){
+
+    if(score == 327){
+        
+        for(var x = 0; x < (defaultSize.x); x++){
+            for(var y = 0; y < (defaultSize.y); y++){
+                ctx.fillStyle = (i == 0)? headColor : snakeColor;
+                ctx.fillRect(x*box, y*box, box, box);
+            
+                ctx.strokeStyle = "black";
+                ctx.strokeRect(x*box, y*box, box, box);
+            }
+        }
+        max = true;
+        clearInterval(game);
+    }
+    
+    var apple_pos = hamCycle2Nums[food.y/box][food.x/box];
+    var head_pos = hamCycle2Nums[snake[0].y/box][snake[0].x/box];
+    var tail_pos = hamCycle2Nums[snake[snake.length-1].y/box][snake[snake.length-1].x/box];
+
+    
+    if(head_pos == 329){
+        next_hamilton = 0;
+    }
+    else{
+        next_hamilton = head_pos + 1;
+    }
+
+    
     //make world
     var world = [[]];
 
-    var cycle = [];
 
-    for(var x = 0; x <= 1349; x++){
-        cycle.push(x);
-    }
-
-    var hamCycle = [];
-    var cycleStart = [22*box, 17*box];
-    var cycleEnd = [22*box, 15*box];
-
+    //find spots
     for(var x = 0; x < (defaultSize.x)*box; x+=box){
         world[x] = [];
         for(var y = 0; y < (defaultSize.y)*box; y+=box){
-            world[x][y] = 0;
-        }
-    }
-    while(hamCycle.length == 0){
-        if(world[cycleStart[0]][cycleStart[1]] == 0){
-            hamCycle = findPath(world, cycleStart, cycleEnd);
-        }
-    }
-    for(rp = 0; rp < hamCycle.length; rp++){
-        if(snakeVision.checked == true){
-                //ctx.fillStyle = 'rgba(255,255,255,0.15)';
-                //ctx.fillRect(hamCycle[rp][0], hamCycle[rp][1], box, box);
+            world[x][y] = 3;
         }
     }
 
-    if(sum == 1350){
-        sum = 0;
-    }
 
-    snakeX = hamCycle[sum][0];
-    snakeY = hamCycle[sum][1];
-    sum++;
+    //find closed spots
+    for(var i = 1; i < snake.length; i++){
+        world[snake[i].x][snake[i].y] = 1;
+    }
 
     
-    
+    function fill(data, x, y, newValue){
+        // get target value
+        var target = data[x][y];
 
+        function flow(x,y){
+            // bounds check what we were passed
+            if( x >= 0 && x < (defaultSize.x)*box && y >= 0 && y < (defaultSize.y)*box){
+                if(data[x][y] === target){
+                    data[x][y] = newValue;
+                    flow(x - box, y);
+                    flow(x + box, y);
+                    flow(x, y - box);
+                    flow(x, y + box);
+                }
+            }
+        }
+        flow(x, y);
+    }
+
+
+    // find open spots
+    fill(world, snake[0].x, snake[0].y, 0);
+
+    if(world[food.x][food.y] != 0){
+        panic = true
+    }
+    else{
+        panic = false;
+    }
+
+
+     var escapeFix = false;
+ 
+
+     for(var i = 1; i < snake.length; i++){
+        world[snake[i].x][snake[i].y] = 1;
+    }
+
+    if(world[food.x][food.y] != 0){
+        panic = true
+    }
+    else{
+        panic = false;
+    }
+
+    //check if the snake enclosed itself
+    var checkIfDead = 0;
+    for(var x = 0; x < (defaultSize.x)*box; x+=box){
+        for(var y = 0; y < (defaultSize.y)*box; y+=box){
+            if(world[x][y] == 0){
+                checkIfDead++;
+            }
+        }
+    }
+    if(checkIfDead == 1){
+        if(mute.checked == false) die.play();
+        clearInterval(game);
+        //resetGame();
+    }
+
+    //255,105,180,.09
+    //highlight selected path
+    for(var x = 0; x < (defaultSize.x)*box; x+=box){
+        for(var y = 0; y < (defaultSize.y)*box; y+=box){
+            if(snakeVision.checked == true){
+                world[snake[0].x][snake[0].y] = 4;
+                if(world[x][y] == 0){
+                    ctx.fillStyle = 'rgba(0,0,255,.09)';
+                    ctx.fillRect(x, y, box, box);  
+                    ctx.restore();
+                }
+                else if(world[x][y] == 3){
+                    ctx.fillStyle = 'rgba(0,0,0,.7)';
+                    ctx.fillRect(x, y, box, box);  
+                    ctx.restore();
+                }
+            }
+        }
+    }
+
+    world[snake[0].x][snake[0].y] = 0;
+    world[snakeTracks[0].x][snakeTracks[0].y] = 0;
+
+    //start of path
+    var pathStart = [snake[0].x, snake[0].y];
+
+    // //end of path
+    if(world[food.x][food.y] == 0){
+        panic = false;
+        var pathEnd = [food.x, food.y];
+    }
+
+
+    if(panic == false){
+        //var pathEnd = [food.x, food.y];
+
+        //current point of path
+        var currentPath = [];
+
+        // begin algorithm  to find shortest path
+
+            while(currentPath.length == 0){
+                if(world[pathStart[0]][pathStart[1]] == 0){
+                    currentPath = findPath(world, pathStart, pathEnd);
+                }
+            }
+
+
+            for(rp = 1; rp < currentPath.length; rp++){
+                if(snakeVision.checked == true){
+                        ctx.fillStyle = 'rgba(255,255,255,0.15)';
+                        ctx.fillRect(currentPath[rp][0], currentPath[rp][1], box, box);
+                }
+            }
+
+        var next_shortest = hamCycle2Nums[currentPath[1][1]/box][currentPath[1][0]/box];
+
+        var takeShortest = false;
+
+        var iterator = next_hamilton;
+
+        for(var i = 0; i < 330; i++){
+            if (iterator == next_shortest){
+                takeShortest = true;
+                break;
+            }
+            else if(iterator == tail_pos){
+                takeShortest = false;
+                break;
+            }
+            else if(iterator == apple_pos){
+                takeShortest = false;
+                break;
+            }
+            else{
+                iterator++;
+                if(iterator == 330){
+                    iterator == 0;
+                }
+            }
+        }
+    }
+    if(takeShortest == true && panic == false && score < 250){
+        snakeX = hamCycle2[next_shortest][0];
+        snakeY = hamCycle2[next_shortest][1];
+    }
+    else{
+        snakeX = hamCycle2[next_hamilton][0];
+        snakeY = hamCycle2[next_hamilton][1];
+    }
+    
+    
+  
+    
     function findPath(world, pathStart, pathEnd){
         // math short cuts
         var abs = Math.abs;
@@ -788,18 +1045,24 @@ if(mode == 4){
         var maxTraversable = 0;
 
         //dimensions;
-        var worldWidth = (45)*box;
-        var worldHeight = (30)*box;
-        var worldSize = (45)*box * (30)*box;
+        var worldWidth = (defaultSize.x)*box;
+        var worldHeight = (defaultSize.y)*box;
+        var worldSize = worldWidth * worldHeight;
         
-    
-        var distance = EuclideanDistance;
+        var distance;
+
+        if(reverse){
+            distance = EuclideanDistance;
+        }
+        else{
+            distance = EuclideanDistance;
+        }
 
         var findNeighbours = function(){};
 
         //mathattan distance formula
-        function DiagonalDistance(point, goal){
-            return max(abs(point.x - goal.x), abs(point.y - goal.y));
+        function ManhattanDistance(point, goal){
+            return abs(point.x - goal.x) + abs(point.y - goal.y);
         }
 
         //euclidean distance
@@ -834,11 +1097,13 @@ if(mode == 4){
             return result;
             
         }
-        
         function canWalkHere(x, y){
-            
+            if(escapeFix == false){
                 return ((world[x] != null) && (world[x][y] != null) && (world[x][y] <= maxTraversable));
-            
+            }
+            else{
+                return ((world[x] != null) && (world[x][y] != null) && (world[x][y] <= 3));
+            }
         
         };
         function Node(parent, point){
@@ -872,45 +1137,67 @@ if(mode == 4){
 
             var length, max, min, i, j;
 
-            // Structure Hamiltonian Cycle Manually
-            for(var i = 21; i > 0; i--){
-                result.push([i*box, 15*box]);
-            }
-            for(var i = 16; i < 30; i++){
-                if(i % 2 == 0){
-                    for(var j = 1; j < 45; j++){
-                        result.push([j*box, i*box]);
+
+            while(length = Open.length){
+                max = worldSize;
+                min = -1;
+
+                for(i = 0; i < length; i++){
+                    if(reverse){
+                        if(Open[i].f > min){
+
+                            min = Open[i].f;
+                            
+                            max = i;    
+        
+                        }
+                    } 
+                    else{
+                        if(Open[i].f < max){
+
+                            max = Open[i].f;
+                            
+                            min = i;
+                        }
                     }
                 }
-                else{
-                    for(var k = 44; k > 0; k--){
-                        result.push([k*box, i*box]);
-                    }
-                }
-            }
-            for(var i = 29; i >= 0; i--){
-                result.push([0*box, i*box]);
-            }
-            for(var i = 0; i < 15; i++){
-                if(i % 2 == 0){
-                    for(var j = 1; j < 45; j++){
-                        result.push([j*box, i*box]);
-                    }
-                }
-                else{
-                    for(var k = 44; k > 0; k--){
-                        result.push([k*box, i*box]);
-                    }
-                }
-            }
-            for(var i = 44; i > 21; i--){
-                result.push([i*box, 15*box]);
-            }
             
+                if(reverse){
+                    myNode =  Open.splice(max, 1)[0];
+                }
+                else{
+                    myNode =  Open.splice(min, 1)[0];
+                }
+
+                if(myNode.value === myPathEnd.value){
+                    myPath = Closed[Closed.push(myNode) - 1];
+                    do{
+                        result.push([myPath.x, myPath.y]);
+                    }
+                    while(myPath = myPath.parent);
+                    AStar = Closed = Open = [];
+                    result.reverse();
+                }
+
+                else{
+                    myNeighbours = Neighbours(myNode.x, myNode.y);
+                    for(i = 0, j = myNeighbours.length; i < j; i++){
+                        myPath = Node(myNode, myNeighbours[i]);
+                        if(!AStar[myPath.value]){
+                            myPath.g = myNode.g + distance(myNeighbours[i], myNode);
+                            myPath.f = myPath.g + distance(myNeighbours[i], myPathEnd);
+                                Open.push(myPath);
+                            AStar[myPath.value] = true;
+                        }
+                    }
+                    Closed.push(myNode);
+                }
+            }
             return result;
         }
         return calculatePath();
         }
+ 
 }
 
      // a function to spawn food
@@ -957,7 +1244,7 @@ if(mode == 4){
         }
 
         pathGone = false;
-
+        
         if(modeSwitch == true){
             mode = 3;
             modeSwitch = false;
@@ -1004,16 +1291,21 @@ if(mode == 4){
     snake.unshift(newHead);
     snakeTracks.unshift(newHead2);
 
-    //scoreboard
+    //scoreboard 100 10 100 100
     sb.fillStyle = "black";
-    sb.fillRect(100,10, 100, 100)
+    sb.fillRect(100, 10, 150, 100)
     sb.fillStyle = "white";
     sb.font = "40px Poppin";
-    sb.drawImage(logo, 36, 0)
-    sb.fillText(" : ", 90, 40);
-    sb.fillText(score, 130, 43);
-
+    sb.drawImage(logo, 40, 0)
+    sb.fillText(" : ", 95, 40);
+    if(max == true){
+        sb.fillText("MAX", 130, 43);
+    }
+    else{
+        sb.fillText(score, 130, 43); //150
+    }
  }
+
 
 
 //call draw function
@@ -1028,28 +1320,29 @@ var aiMode = document.getElementById("ai");
 var aiMode2 = document.getElementById("ai2");
 var aiMode3 = document.getElementById("ai3");
 var aiMode4 = document.getElementById("ai4");
+var aiMode5 = document.getElementById("ai5");
 
 function changeSpeed(){
 
-    if(defaultSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true)){
-        speed = 15;
+    if(defaultSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true || aiMode5.checked == true)){
+        speed = 30; //15
     }
     else if(defaultSpeed.checked == true){
         speed = 65;
     }
-    else if(slowSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true)){
-        speed = 30;
+    else if(slowSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true || aiMode5.checked == true)){
+        speed = 50;
     }
     else if(slowSpeed.checked == true){
-        speed = 110;
+        speed = 100;
     }
-    else if(fastSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true)){
-        speed = 10;
+    else if(fastSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true || aiMode5.checked == true)){
+        speed = 45;
     }
     else if(fastSpeed.checked == true){
         speed = 35;
     }
-    else if(ultraSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true)){
+    else if(ultraSpeed.checked == true && (aiMode3.checked == true || aiMode4.checked == true || aiMode5.checked == true)){
         speed = .0001;
     }
     else if(ultraSpeed.checked == true){
@@ -1073,6 +1366,9 @@ function changeMode(){
     else if(aiMode4.checked == true){
         mode = 4;
     }
+    else if(aiMode5.checked == true){
+        mode = 5;
+    }
 }
 
 function resetGame(){
@@ -1083,10 +1379,12 @@ function resetGame(){
     changeSpeed();
     changeMode();
     clearInterval(game);
+    hamilton = true;
     score = 0;
     direction = 0;
     snake.length = 1;
     modeSwitch = false;
+    max = false;
 
     if(aiMode2.checked == true || aiMode3.checked == true){
         
