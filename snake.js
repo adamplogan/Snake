a8d7996aba2b21e4dedc0ba36a3046e4de5f89dc
Adam <81320110+adamplogan@users.noreply.github.com>
2022-03-25 15:21:55 -0400

    Update snake.js

--- snake.js
+++ snake.js
@@ -1027,7 +1027,7 @@ if(mode == 5){
             }
         }
     }
-    if(takeShortest == true && panic == false && score < 150){
+    if(takeShortest == true && panic == false && score < 165){
         snakeX = hamCycle2[next_shortest][0];
         snakeY = hamCycle2[next_shortest][1];
     }
